b80872c4e2cde84016fea7a3e06e09cab58d46e6
Guilherme Matos <datwo.matos@gmail.com>
2017-11-21 17:17:06 +0000

    basic views

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,2 +1,28 @@
 class IssuesController < ApplicationController
+
+  def index
+    @issues = Issues.all
+  end
+
+  def show
+    @issue = Issue.find(params[:id])
+  end
+
+  def new
+    @issue = Issue.new
+  end
+
+  def create
+    @issue = Issue.new
+  end
+
+  def edit
+    @issue = Issue.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
